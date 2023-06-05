c7c05aebdbbff7e6e6d60e302562c18e1f1795e9
Baiba Niedole <nbaiba@inbox.lv>
2023-06-05 21:03:22 +0300

    small changes

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 my_name = "Baiba "
 seconds_in_day = 86400
+
+# Izdrukājiet savu vārdu 5 reizes.
 print(my_name * 5)
+
+# Izdrukājiet savu vārdu 7 reizes.
 print(my_name * 7)
+
+# Aprēķiniet cik sekundes būs šogad (365 dienas šogad!)
 print(seconds_in_day * 365)
+
+#Aprēķiniet googol, tas ir 10 - 100tajā pakāpē (10**100)
 print(10 ** 100)
+
+#Izdrukājiet Banananana - tikai izmantojot Ba un na (nevis pilnu vārdu)
 print("Ba" + "na" * 4)
