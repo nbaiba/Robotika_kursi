6a52e1d471f4f3c21bf8fed0c972865707b4503d
Baiba Niedole <nbaiba@inbox.lv>
2023-06-07 20:35:06 +0300

    small fix

--- 07_06_uzdevums.py
+++ 07_06_uzdevums.py
@@ -9,7 +9,7 @@ year_now = datetime.datetime.now().year
 name = input("Whats your name? => ")
 
 # # ask age, asign to int
-age = int(input(f"Nice to meet you {name} how old are you? => "))
+age = int(float(input(f"Nice to meet you {name} how old are you? => ")))
 
 # # print with calculations 
 print(f"{name}, after {100 - age} years you will be 100 years old, and that will be in year {100 - age + year_now}")
@@ -19,13 +19,12 @@ print(f"{name}, after {100 - age} years you will be 100 years old, and that will
 # ###
 
 # # ask measurements
-width = int(input("What is width of room? => "))
-height = int(input("What is height of room? => "))
-length = int(input("What is length of room? => "))
+width = float(input("What is width of room? => "))
+height = float(input("What is height of room? => "))
+length = float(input("What is length of room? => "))
 
 # calculate and print volums
-
-print(f"Total volume is {width * height * length}")
+print(f"Total volume is {round(width * height * length, 2)}")
 
 
 ###
@@ -33,7 +32,7 @@ print(f"Total volume is {width * height * length}")
 ###
 
 # ask temperature, asign to variable
-temperature = int(input("Whats temperature right now in Celsius? => "))
+temperature = float(input("Whats temperature right now in Celsius? => "))
 
 # print farenheits with calculation
-print(f"In Farenheit that will be {32 + temperature * (9/5)}")
+print(f"In Farenheit that will be {round(32 + temperature * (9/5), 2)}")
